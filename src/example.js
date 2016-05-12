6bdbb9bceee5dc5e429a18d04c088b4fe5ff1a68
Daniel Moll <dan.moll@icloud.com>
2016-05-12 10:28:54 +0100

    chore: updated example to use tabs that support react 0.14+

--- src/example.js
+++ src/example.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import SampleText from './sampletext';
-import Tabs from 'react-simpletabs';
+import ReactTabs from 'react-tabs';
 
+const Tab = ReactTabs.Tab;
+const Tabs = ReactTabs.Tabs;
+const TabList = ReactTabs.TabList;
+const TabPanel = ReactTabs.TabPanel;
 const fonts = [
   [ 'sans', '300', '', 'EconSans' ],
   [ 'sans', '', '', 'EconSans' ],
@@ -11,7 +15,7 @@ const fonts = [
   [ 'serif', '500', '', 'MiloSerifPro' ],
   [ 'serif', '', 'italic', 'MiloSerifPro' ],
 ];
-const eachSample = fonts.map((fontFamily) => {
+const panels = fonts.map((fontFamily) => {
   const [ kind, modifier, fontStyle, family ] = fontFamily;
   const classes = [
     `example__${ kind }-text`,
@@ -25,7 +29,7 @@ const eachSample = fonts.map((fontFamily) => {
     style.fontStyle = fontStyle;
   }
   return (
-    <Tabs.Panel title={`${ family } ${ modifier } ${ fontStyle }`} key={`typography-${ fontFamily.join('x') }`}>
+    <TabPanel key={`panel-typography-${ fontFamily.join('x') }`}>
       <h2>Sample for font-family: {fontFamily.join(' ')}</h2>
       <div
         className={classes}
@@ -33,11 +37,26 @@ const eachSample = fonts.map((fontFamily) => {
         data-font={family.toLowerCase().replace(/ /g, '-')}
       ><SampleText /></div>
       <hr />
-    </Tabs.Panel>
+    </TabPanel>
+  );
+});
+
+const tabs = fonts.map((fontFamily) => {
+  const [ modifier, fontStyle, family ] = fontFamily;
+  return (
+    <Tab key={`tab-typography-${ fontFamily.join('x') }`}>
+      <span>{family} {modifier} {fontStyle}</span>
+    </Tab>
   );
 });
 export default (
-  <Tabs className="library--example-tabs">
-    {eachSample}
+  <Tabs
+    className="library--example-tabs"
+    selectedIndex={0}
+  >
+    <TabList>
+      {tabs}
+    </TabList>
+    {panels}
   </Tabs>
 );
